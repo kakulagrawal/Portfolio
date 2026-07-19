3bc570d9862292849e6b347d0ceba27cffd0cd81
Kakul Agrawal <kakulagrawal32@gmail.com>
2026-07-19 11:56:29 +0530

    Hero component layout for improved responsiveness

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -12,14 +12,29 @@ const Hero = () => {
       <HeroBackground />
 
       {/* Main Hero Layout */}
-      <div className="hero-wrapper flex gap-20">
+      <div className="hero-wrapper mx-auto
+    flex
+    flex-col
+    items-center
+    justify-center
+    gap-12
+    px-6
+    pt-12
+    pb-10
+    max-w-7xl
+    min-h-screen
+
+    lg:flex-row
+    lg:justify-between
+    lg:gap-20
+    lg:px-8">
         {/* Left Content */}
-        <div className="hero-content ml-12">
+        <div className="hero-content w-full max-w-2xl text-center lg:text-left ">
           <HeroContent />
         </div>
 
         {/* Right Avatar */}
-        <div className="hero-avatar-container mt-20">
+        <div className="hero-avatar-container justify-center  lg:justify-end ">
           <HeroAvatar />
         </div>
       </div>
